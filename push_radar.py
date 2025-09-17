2b7c057395beabc7f73b45e69ebfeefcdfc8021c
COLM MONAHAN <CM329219@falmouth.ac.uk>
2025-09-17 11:19:57 +0100

    add: radar task

---
+++ push_radar.py
@@ -0,0 +1,15 @@
+from bot import Bot
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.robot.straight(480)
+    bot.robot.turn(-30)
+    bot.robot.straight(400)
+    bot.robot.turn(-150)
+    bot.robot.straight(600)
+# fowards
+# turn left
+# forwards
+# backwards
+# turn right
+# forwards
